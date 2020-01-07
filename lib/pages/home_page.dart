40e6056740e3be5bc7a41627fdaa5d36f5c7b496
AdriRed <adriamilmarcos@gmail.com>
2020-01-07 19:10:40 +0100

    removed comments

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,15 +9,6 @@ import 'package:flutter/material.dart';
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -37,16 +28,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     log("Showing " + _databaseList.length.toString() + " items");
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text("Send to database"),
       ),
       body: Center(
